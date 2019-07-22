957ed63958d267d79bbe34e9e506e2da58e49c63
serg-vostrikov-bitrix <52790482+serg-vostrikov-bitrix@users.noreply.github.com>
2019-07-22 16:26:08 +0200

    Add files via upload

---
+++ src/install.php
@@ -0,0 +1,23 @@
+<?php
+require_once (__DIR__.'/crest.php');
+
+$result = CRest::installApp();
+if($result['rest_only'] === false):?>
+<head>
+	<script src="//api.bitrix24.com/api/v1/"></script>
+	<?if($result['install'] == true):?>
+	<script>
+		BX24.init(function(){
+			BX24.installFinish();
+		});
+	</script>
+	<?endif;?>
+</head>
+<body>
+	<?if($result['install'] == true):?>
+		installation has been finished
+	<?else:?>
+		installation error
+	<?endif;?>
+</body>
+<?endif;
