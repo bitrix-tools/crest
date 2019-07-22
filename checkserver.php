7e023b668b5242d21649a67458eed2ef09033b54
serg-vostrikov-bitrix <52790482+serg-vostrikov-bitrix@users.noreply.github.com>
2019-07-22 15:57:35 +0200

    Add files via upload
    initial upload

---
+++ checkserver.php
@@ -0,0 +1,4 @@
+<?
+require_once (__DIR__.'/crest.php');
+
+CRest::checkServer();
---
+++ index.php
@@ -0,0 +1,8 @@
+<?
+require_once (__DIR__.'/crest.php');
+
+$result = CRest::call('profile');
+
+echo '<pre>';
+	print_r($result);
+echo '</pre>';
