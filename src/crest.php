b3bb718839e42e6dc3facda0f13558a692029def
Vlad Bazhanov <i@vlads.name>
2020-06-25 13:40:15 +0200

    up version

--- src/crest.php
+++ src/crest.php
@@ -2,7 +2,7 @@
 	require_once (__DIR__.'/settings.php');
 
 	/**
-	 *  @version 1.34
+	 *  @version 1.35
 	 *  define:
 	 *      C_REST_WEB_HOOK_URL = 'https://rest-api.bitrix24.com/rest/1/doutwqkjxgc3mgc1/'  //url on creat Webhook
 	 *      or
